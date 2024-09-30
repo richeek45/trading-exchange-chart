56cb0872c83cf1e20bacdd83e0355839cb6bc2fe
richeek45 <richeek45@gmail.com>
2024-09-30 23:10:09 +0530

    adding tadding titles

--- src/components/ChartSwitcher.tsx
+++ src/components/ChartSwitcher.tsx
@@ -6,13 +6,17 @@ const ChartSwitcher = () => {
 
   return (
     <div>
-    <div className="mb-6">
-      <h2 className="text-4xl font-extrabold text-indigo-600 tracking-wide border-b-4 border-indigo-400 pb-3">
-        Latest Trading Data
-      </h2>
-      <div className="text-9xl">Now</div>
-    </div>
+      <div className="mb-6">
+        <h2 className="text-4xl font-extrabold text-indigo-600 tracking-wide border-b-4 border-indigo-400 pb-3">
+          Latest Trading Data
+        </h2>
+      </div>
       <Candlestick MODE="LATEST" />
+      <div className="mb-6">
+        <h2 className="text-4xl font-extrabold text-indigo-600 tracking-wide border-b-4 border-indigo-400 pb-3">
+          Historical Trading Data
+        </h2>
+      </div>
       <Candlestick MODE="HISTORICAL" />
     </div>
   )
